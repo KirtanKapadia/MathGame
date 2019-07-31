c54dd4b21314fe395d73f821a9bb18c6da7affb4
KirtanKapadia <kirtankapadia13@gmail.com>
2019-07-31 22:18:09 +0530

    2nd commit With Score View

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,19 +16,28 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView Timer = findViewById(R.id.timer);
         Timer.setVisibility(View.VISIBLE);
-//        TextView Score = findViewById(R.id.Score);
-//        Score.setVisibility(View.VISIBLE);
+        TextView Score = findViewById(R.id.Score);
+        Score.setVisibility(View.VISIBLE);
 
 
-        new CountDownTimer(30000, 1000) {
+        new CountDownTimer(30100, 1000) {
             @Override
             public void onTick(long l) {
-                 Timer.setText(String.valueOf((int)l/1000) + "s");
+                int timeLeft = (int)l/1000;
+                String tLeft;
+                if(timeLeft < 10) {
+                    tLeft = "0" + String.valueOf(timeLeft) + "s";
+                }
+                else
+                    tLeft = String.valueOf(timeLeft) + "s";
+                 Timer.setText(tLeft);
             }
 
             @Override
             public void onFinish() {
+
                 Timer.setText("30s");
+                Toast.makeText(MainActivity.this, "Ooops!!Time is up!!", Toast.LENGTH_SHORT).show();
             }
         }.start();
     }
